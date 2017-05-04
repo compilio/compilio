4e4c181ab8d7d74b39920076c57f63a05f18b863
Quentin de Longraye <quentin@dldl.fr>
2017-05-04 16:53:39 +0200

    add base files for templating

--- compilio/settings.py
+++ compilio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'compilio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['compiler/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
