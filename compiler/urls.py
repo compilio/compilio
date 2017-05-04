25f4bf5af0365cb88e9d8076ab50129e8147113e
Victor Le <levictor3000@hotmail.fr>
2017-05-04 22:01:33 +0200

    Login form

--- compiler/urls.py
+++ compiler/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('^', include('django.contrib.auth.urls')),
 ]
