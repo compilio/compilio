8ea0bb439457604cd3e42b8ee35741a96b077e20
Quentin de Longraye <quentin@dldl.fr>
2017-05-06 15:21:39 +0200

    add working authentication

--- compilio/compiler/views.py
+++ compilio/compiler/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 
 def index(request):
     return render(request, 'compiler/index.html')
+
+
+def tasks(request):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    return render(request, 'compiler/tasks.html')
--- compilio/urls.py
+++ compilio/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login', auth_views.login, {'template_name': 'user/login.html'}, name='login'),
+    url(r'^logout', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^', include('compilio.compiler.urls')),
     url(r'^admin/', admin.site.urls),
 ]
