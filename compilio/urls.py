02dcecdf84012ce49ce1a14ffd760b8f5d5c097c
Quentin de Longraye <quentin@dldl.fr>
2017-05-06 14:32:52 +0200

    fix login url

--- compilio/urls.py
+++ compilio/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login', auth_views.login, {'template_name': 'user/login.html'}, name='login'),
     url(r'^', include('compilio.compiler.urls')),
     url(r'^admin/', admin.site.urls),
 ]
