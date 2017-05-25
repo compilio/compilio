c9d868e561143ce4ba85b277f764096e331c54fa
Victor Le <levictor3000@hotmail.fr>
2017-05-25 09:27:38 +0200

    Task save get outputfile

--- compilio/compiler/api.py
+++ compilio/compiler/api.py
@@ -104,21 +104,6 @@ def upload(request):
     return JsonResponse({'ok': uploaded_file_url})
 
 
-def save_output_file(task_id, res):
-    filename = 'uploads/tasks/' + task_id + '/output.zip'
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w+b') as f:
-        f.write(res.content)
-
-
-def __get_save_output_files(task_object):
-    res = requests.get(task_object.server_compiler.server.ip + ':'
-                       + str(task_object.server_compiler.port)
-                       + '/get_output_files?id=' + task_object.id)
-    if res.status_code == 200:
-        save_output_file(task_object.id, res)
-
-
 @csrf_exempt
 def task(request):
     try:
@@ -135,7 +120,7 @@ def task(request):
         return JsonResponse(res_json)
 
     if res_json['state'] == 'SUCCESS':
-        __get_save_output_files(task_object)
+        task_object.get_save_output_files()
 
     return JsonResponse(res_json)
 
