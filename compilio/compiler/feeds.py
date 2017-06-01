e0af9e217b1bf42670d21e7496eacb899000749c
Victor Le <levictor3000@hotmail.fr>
2017-06-01 17:28:39 +0200

    Fix items rss

--- compilio/compiler/feeds.py
+++ compilio/compiler/feeds.py
@@ -9,13 +9,16 @@ class LatestEntriesFeed(Feed):
     title = "Last tasks on Compilio"
     link = "/tasks"
     description = "Last added tasks on Compilio."
+    tasks = None
 
     def get_object(self, request, username):
         user = User.objects.get(username=username)
+        self.tasks = Task.objects.filter(owners__id=user.id).order_by('id')[:5]
         return Task.objects.filter(owners__id=user.id).order_by('id')[:5]
 
-    @staticmethod
-    def items():
+    def items(self):
+        if self.tasks:
+            return self.tasks
         return Task.objects.order_by('id')[:5]
 
     def item_title(self, item):
