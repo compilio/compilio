ebd8d65597a2027aed92d9bae80303f93e1a59df
Victor Le <levictor3000@hotmail.fr>
2017-06-01 17:00:20 +0200

    RSS feed by username

--- compilio/compiler/feeds.py
+++ compilio/compiler/feeds.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
+
 from .models import Task
 
 
@@ -7,6 +9,10 @@ class LatestEntriesFeed(Feed):
     title = "Last tasks on Compilio"
     link = "/tasks"
     description = "Last added tasks on Compilio."
+
+    def get_object(self, request, username):
+        user = User.objects.get(username=username)
+        return Task.objects.filter(owners__id=user.id).order_by('id')[:5]
 
     @staticmethod
     def items():
